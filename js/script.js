ed7813312aab29531d9099788f6f40136bf9339f
SamuMeneDev <samuelsantiago2222@gmail.com>
2025-08-01 11:14:04 -0300

    Lógica completa

--- js/script.js
+++ js/script.js
@@ -1,48 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-    updateList();
-});
-
-let btnDel = document.querySelector('#delTask');
-const btnAdd = document.querySelector('#addTask');
-const listaEl = document.querySelector('#list');
+const btnAdd = document.querySelector("#addTask");
+const listaEl = document.querySelector("#list");
 const inputEl = document.querySelector("#input");
-
-const tasks = [
-    {
-        id: 1,
-        title: "Jogar bola",
-        isCompleted: false,
-    },
-    {
-        id: 2,
-        title: "Estudar Javascript",
-        isCompleted: false,
-    },
+let tasks = [
+  {
+    id: 1,
+    title: "Jogar bola",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: "Estudar Javascript",
+    isCompleted: false,
+  },
 ];
+document.addEventListener("DOMContentLoaded", function () {
+  updateList();
+});
 function updateList() {
     listaEl.innerHTML = `<ul id="list"></ul>`;
-    tasks.forEach(task => {
-        const el = document.createElement("li");
-        el.innerHTML = `
-            <li id="${task.id}">
-                <button id="${task.id}">${task.title}</button>
-                <button id="delTask">Deletar</button>
-            </li>`;
-        listaEl.appendChild(el);
-    });
-    btnDel = document.querySelector('#delTask');
-}
+    if(tasks.length==0) {
+        listaEl.innerHTML = `<ul id="list">Adicione uma tarefa</ul>`;
+    } else {
+        tasks.forEach((task) => {
+            const el = document.createElement("li");
+            el.innerHTML = `
+                <li id="${task.id}">
+                    <button id="${task.id}" onclick="checkTask(this)">${task.isCompleted?`<strike>${task.title}</strike>`:task.title}</button>
+                    <button id="${task.id}" onclick="deletar(this)">Deletar</button>
+                </li>`;
+            listaEl.appendChild(el);
+        });
+    }
 
-btnAdd.addEventListener("click", function() {
-    const text = inputEl.value;
+}
+function deletar(e) {
+    tasks.forEach(task => {
+        if (e.id == task.id) {
+            tasks = tasks.filter(el => el.id!=task.id);
+        }
+    });
+    updateList();
+}
+function checkTask(e) {
+    tasks.forEach(task => {
+        if (e.id == task.id) {
+            task.isCompleted = !task.isCompleted;
+        }
+    });
+    updateList();
+}
+btnAdd.addEventListener("click", function () {
+  const text = inputEl.value;
+  if (text.trim() != "") {
     const task = {
-        id: tasks.length>0?tasks[tasks.length-1].id+1:1,
-        title: text,
-        isCompleted: false,
+      id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+      title: text,
+      isCompleted: false,
     };
     tasks.push(task);
     updateList();
+    inputEl.value = "";
+  }
 });
-btnDel.addEventListener("click", function() {
-    
-})
+
